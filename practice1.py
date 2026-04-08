a296566c2ae7e7bcbd5e16d7ac414a45e273a3cc
Mahnaj Binta Rahman <156027368+mahnajbintarahman@users.noreply.github.com>
2026-04-08 22:04:43 +0600

    Add files via upload
    practice problem base on day1

---
+++ practice1.py
@@ -0,0 +1,34 @@
+# #here variable 
+x = 10 
+y = 3.5 
+name = "mahnaj"
+is_ok = True
+print(name)
+print(f"there is a {x} ice cream")
+print(f"i want to achive {y} CGPA")
+
+# #here we input /output 
+name2 = input("Enter your name:")
+age =input("enter your age") 
+
+print("hello",name2)
+print("my age is :", age)
+
+#  if-else condition 
+age2 = 23
+
+if age2 >= 18:
+    print("Adult")
+else : 
+    print("Minor")
+
+# for loop 
+for i in range(5):
+    print(i)
+    
+
+
+
+
+
+
